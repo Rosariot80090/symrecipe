ed822c1b43ee51b664e077e60bb42a9b184da589
Rosariot80090 <grosariot@yahoo.fr>
2023-06-07 14:30:18 +0200

    premier envoi

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', 'home.index', methods: ['GET'])]
+    //home.index=home de HomController et index nom de la function
+    
+    public function index(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+}
